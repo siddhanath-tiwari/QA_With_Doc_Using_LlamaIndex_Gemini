7ec1b096b203b3cd50e9d9052802e4f37b725e5c
siddhanth-tiwari <pratimatiwari8299@gmail.com>
2024-06-23 10:33:22 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'siddhanath-tiwari',
+    author_email= 'pratimatiwari8299@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
